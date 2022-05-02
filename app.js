442a23da69edce9c95fae67d39e160c71071efe2
Alexander Gentile <alexdgentile8@gmail.com>
2022-05-01 22:11:56 -0400

    Add procfile and update ports

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/merchStoreDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://Merchuser101:ilovemerch27@merch-store.i0rwu.mongodb.net/merchStoreDB", {useNewUrlParser: true});
 
 
 
@@ -353,6 +353,11 @@ app.post("/checkout", function(req, res){
 
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started Successfully");
 });
